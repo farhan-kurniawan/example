0d1935019f61fe080ba240184e9c8c0873d14fdf
farhan-kurniawan <farhankurniawan200804@gmail.com>
2024-05-31 19:00:54 +0700

    tugas besar praktikum pemograman.py

--- tugas besar praktikum pemograman.py
+++ tugas besar praktikum pemograman.py
@@ -14,17 +14,17 @@ print("10. Es jeruk: Rp. 5000")
 print("11. Es buah: Rp. 10.000")
 
 def totalsemuahargamenu(menu, hargamenunasgor):
-  totalharga = 0
-  for pesanan in menu:
-    totalharga += hargamenunasgor[pesanan -1]
-  return totalharga
+    totalharga = 0
+    for pesanan in menu:
+        totalharga += hargamenunasgor[pesanan -1]
+    return totalharga
 
 def totalsemua():
-  hargamenunasgor = [13000, 15000, 15000, 17000, 22000, 13000, 13000, 13000, 3000, 5000, 10000]
-  menu = []
+    hargamenunasgor = [13000, 15000, 15000, 17000, 22000, 13000, 13000, 13000, 3000, 5000, 10000]
+    menu = []
 
   
-  while True:
+    while True:
         pesanan = input("Masukkan nomor menu yang ingin dipesan (ketik 0 untuk total harga yang harus dibayar): ")
 
         if pesanan == "0":
@@ -35,46 +35,46 @@ def totalsemua():
             print("mohon maaf pesanan yang anda input tidak ada di menu. Silahkan masukan kembali nomor pesanan yang ada di menu!")
             continue
 
-        menu.append(pesanan)
+      menu.append(pesanan)
 
-    if len(menu) == 0:
+  if len(menu) == 0:
       print("tidak ada yang dipesan")
-    else:
+  else:
       totalharga = totalsemuahargamenu(menu, hargamenunasgor)
       print("Total harga: Rp.", totalharga)
 
       menu.sort()
       print("Pesanan Anda: ")
       for pesanan in menu:
-        if pesanan <=5 :
-          kategori = "Makanan"
-        else pesanan >=5 :
-          kategori = "Minuman"
+          if pesanan <=5 :
+              kategori = "Makanan"
+          else pesanan >=5 :
+              kategori = "Minuman"
         
-        if pesanan == 1:
-          item = "Nasi goreng biasa"
-        elif pesanan == 2:
-          item = "Nasi goreng sosis"
-        elif pesanan == 3:
-          item = "Nasi goreng bakso"
-        elif pesanan == 4:
-          item = "Nasi goreng ati ampela"
-        elif pesanan == 5:
-          item = "Nasi goreng Spesial"
-        elif pesanan == 6:
-          item = "Kwetiau goreng"
-        elif pesanan == 7:
-          item = "Mie goreng"
-        elif pesanan == 8:
-          item = "Mie kuah"
-        elif pesanan == 9:
-          item = "Es teh"
-        elif pesanan == 10:
-          item = "Es jeruk"
-        elif pesanan == 11:
-          item = "Es buah"
+          if pesanan == 1:
+              item = "Nasi goreng biasa"
+          elif pesanan == 2:
+              item = "Nasi goreng sosis"
+          elif pesanan == 3:
+              item = "Nasi goreng bakso"
+          elif pesanan == 4:
+              item = "Nasi goreng ati ampela"
+          elif pesanan == 5:
+              item = "Nasi goreng Spesial"
+          elif pesanan == 6:
+              item = "Kwetiau goreng"
+          elif pesanan == 7:
+              item = "Mie goreng"
+          elif pesanan == 8:
+              item = "Mie kuah"
+          elif pesanan == 9:
+              item = "Es teh"
+          elif pesanan == 10:
+              item = "Es jeruk"
+          elif pesanan == 11:
+              item = "Es buah"
         
-        print(f"- {kategori}: {item}")
+          print(f"- {kategori}: {item}")
 
 totalsemua()
         
